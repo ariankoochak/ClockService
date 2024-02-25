f428cac7f5141a70a6209b33c4b3b63f08c742ef
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-25 10:58:59 +0330

    frontend- write change login role

--- Front-End/clock-service/src/layouts/LoginPage/LoginPage.jsx
+++ Front-End/clock-service/src/layouts/LoginPage/LoginPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+  let roles = ['مشتری','اپراتور','تعمیرکار']
+  const [loginRole,setLoginRole] = useState('مشتری');
+  const generateOptionsButton = ()=>{
+    roles = roles.filter((role)=>{
+      return role !== loginRole
+    })
+    return (
+        <>
+            <button onClick={handleClickNewLoginRole}>ورود {roles[0]}</button>
+            <button className="set-16-left-margin" onClick={handleClickNewLoginRole}>ورود {roles[1]}</button>
+        </>
+    );
+  }
+  const handleClickNewLoginRole = (e)=>{
+    const clickedRoleName = e.target.innerHTML.split(" ").pop();
+    setLoginRole(clickedRoleName)
+  }
   return (
     <div className="login-page-main-div">
         <div className="header">
          <h4>سامانه ی گزارش خرابی ساعت</h4>
-         <span>ورود به حساب کاربری مشتری</span>
+         <span>ورود به حساب کاربری {loginRole}</span>
         </div>
         <div className="main-form">
           <input type="text" placeholder='نام کاربری'/>
@@ -18,8 +35,7 @@ export default function LoginPage() {
           <div className="line"></div>
         </div>
         <div className="options">
-          <button>ورود اپراتور</button>
-          <button className='set-16-left-margin'>ورود تعمیرکار</button>
+          {generateOptionsButton()}
         </div>
     </div> 
   )
