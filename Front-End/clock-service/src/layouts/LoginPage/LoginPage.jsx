2dd2262538f60bd2042157946c9f275b2b798ffc
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-25 10:44:00 +0330

    frontend-add font and make login style

--- Front-End/clock-service/src/layouts/LoginPage/LoginPage.jsx
+++ Front-End/clock-service/src/layouts/LoginPage/LoginPage.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div className="login-page-main-div">
+        <div className="header">
+         <h4>سامانه ی گزارش خرابی ساعت</h4>
+         <span>ورود به حساب کاربری مشتری</span>
+        </div>
+        <div className="main-form">
+          <input type="text" placeholder='نام کاربری'/>
+          <input type="text" placeholder='رمز عبور' />
+          <button>ورود</button>
+        </div>
+        <div className="splitter">
+          <div className="line"></div>
+          <span>یا</span>
+          <div className="line"></div>
+        </div>
+        <div className="options">
+          <button>ورود اپراتور</button>
+          <button className='set-16-left-margin'>ورود تعمیرکار</button>
+        </div>
+    </div> 
   )
 }
