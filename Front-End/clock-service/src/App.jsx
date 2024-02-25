1cf34d0b82161ff1760da6055d12cbf05c44e2c4
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-25 09:22:47 +0330

    frontend- fix go to login page

--- Front-End/clock-service/src/App.jsx
+++ Front-End/clock-service/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import "./assets/styles/global.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './layouts/LoginPage/LoginPage';
+import HomePage from './layouts/HomePage/HomePage';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="/Login" element={<LoginPage />}></Route>
+          </Routes>
+      </BrowserRouter>
+  );
 }
