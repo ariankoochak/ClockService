aed5df7629307fb6635905c370127fe7229ee0cc
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-27 12:53:31 +0330

    frontend-finish chat page style

--- Front-End/clock-service/src/components/TicketChats/TicketChats.jsx
+++ Front-End/clock-service/src/components/TicketChats/TicketChats.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { exitToTicketChatMode } from '../../utils/store/slices/ticketChatMode';
 
 export default function TicketChats() {
+  const dispatch = useDispatch()
+  const handleCancelFormClick = () => {
+      dispatch(exitToTicketChatMode());
+  };
   return (
-    <div>TicketChats</div>
-  )
+      <div className="chat-page">
+          <div className="exit-chat">
+              <button onClick={handleCancelFormClick}>بازگشت</button>
+          </div>
+
+          <div className="chats-box"></div>
+
+          <div className="chat-input">
+              <div className="send-btn">
+                  <button>ارسال</button>
+              </div>
+              <div className="chat-inp">
+                  <input type="text" placeholder='متن پیام...'/>
+              </div>
+          </div>
+      </div>
+  );
 }
