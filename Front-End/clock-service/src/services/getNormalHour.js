dc071fdef0ac5d2f410570cf6b1412094b444e3a
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-28 13:07:07 +0330

    fix under 10 hours and  minutes

--- Front-End/clock-service/src/services/getNormalHour.js
+++ Front-End/clock-service/src/services/getNormalHour.js
@@ -1,4 +1,11 @@
+function fixUnder10Number(inp){
+    if(inp < 10){
+        return `0${inp}`
+    }
+    return inp
+}
+
 export default function getNormalHour(date) {
     const d = new Date(date);
-    return `${d.getHours()}:${d.getMinutes()}`;
+    return `${fixUnder10Number(d.getHours())}:${fixUnder10Number(d.getMinutes())}`;
 }
