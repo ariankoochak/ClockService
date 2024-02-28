013dd1394cbb7810791945c6ba8d4defa0c33a7e
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-28 13:03:57 +0330

    frontend-make shamsi date

--- Front-End/clock-service/src/services/getNormalDate.js
+++ Front-End/clock-service/src/services/getNormalDate.js
@@ -1,4 +1,4 @@
 export default function getNormalDate(date){
     const d = new Date(date);
-    return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`;
+     return new Date().toLocaleDateString("fa-IR");
 }
