ae32874b0e39310e8b729c81a325f260604be274
Arian <45326611+ariankoochak@users.noreply.github.com>
2024-02-28 10:41:41 +0330

    frontend - add fontawesme to project

--- Front-End/clock-service/src/index.jsx
+++ Front-End/clock-service/src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from "react-redux";
 import { store } from "./utils/store/store";
+import "./assets/fontawesome/css/fontawesome.css";
+import "./assets/fontawesome/css/brands.css";
+import "./assets/fontawesome/css/solid.css";
+import "./assets/fontawesome/css/v5-font-face.css";
+import "./assets/fontawesome/css/all.css";
+import "./assets/fontawesome/css/v4-shims.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
